cb440ab3677da89931f037418041b48d33524f5a
Jason Rodriguez <jason@dittowords.com>
2022-06-21 20:22:26 -0700

    Initial Commit

---
+++ Dittobnb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dittobnb
+//
+//  Created by Jason Rodriguez on 6/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dittobnb/DittobnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittobnbApp.swift
+//  Dittobnb
+//
+//  Created by Jason Rodriguez on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct DittobnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
